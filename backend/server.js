b9327cdbfb88106ab6cac03c935c26c527556e87
jimbosl1ce <jdhogerty@gmail.com>
2021-08-25 09:27:21 -0700

    completed SHIPPING component + component state

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+// eslint-disable-next-line no-unused-vars
 import colors from "colors";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
